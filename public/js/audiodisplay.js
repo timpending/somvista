111040f7cfa6ac278a351fc3a00fbadcb66f5957
Tim Pender <mrtimpender@gmail.com>
2016-10-04 22:22:31 -0600

    added d3, testing zone on index

--- public/js/audiodisplay.js
+++ public/js/audiodisplay.js
@@ -1,13 +1,20 @@
 function drawBuffer( width, height, context, data ) {
+    console.log(data);
+    console.log('*******');
+    console.log(data.length);
+    console.log('888888888');
     var step = Math.ceil( data.length / width );
     var amp = height / 2;
     context.fillStyle = "silver";
     context.clearRect(0,0,width,height);
+    console.log(step);
     for(var i=0; i < width; i++){
         var min = 1.0;
         var max = -1.0;
         for (j=0; j<step; j++) {
-            var datum = data[(i*step)+j]; 
+            var datum = data[(i*step)+j];
+            // datum = array [ (1 * integer of (length/width) ) + 1 ]
+            //
             if (datum < min)
                 min = datum;
             if (datum > max)
--- public/js/main.js
+++ public/js/main.js
@@ -42,14 +42,6 @@ function gotBuffers( buffers ) {
     var canvas = document.getElementById( "wavedisplay" );
 
     drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
-    console.log('88888');
-    console.log(buffers[0]);
-    console.log('88888');
-    console.log(buffers[1]);
-    console.log('~~~~~~~~~~');
-    console.log('buffers lengths');
-    console.log(buffers[0].length);
-    console.log(buffers[1].length);
 
 
     // the ONLY time gotBuffers is called is right after a new recording is completed -
