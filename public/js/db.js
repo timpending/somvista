643f79a9d4c839173a6a77d94a3b5f7ccaebb3ba
Tim Pender <mrtimpender@gmail.com>
2016-10-10 10:59:25 -0600

    adjusting client side with db call on Title button

--- public/js/db.js
+++ public/js/db.js
@@ -1,8 +1,26 @@
-var mainText = document.getElementById('mainText')
-var mainButton = document.getElementById('mainButton')
 var firebaseDB = firebase.database().ref()
 
 
 function saveToDB() {
-  firebaseDB.child("TEST").set('First Values')
+  var mainText = document.getElementById('mainText')
+  var human = document.getElementById('human')
+  var mainButton = document.getElementById('mainButton')
+  var msg = mainText.value
+  var human = human.value
+  firebaseDB.child(human).set(msg)
 }
+
+function getTitle() {
+  var newTitle = firebase.database().ref().child("Title")
+  var title = document.getElementById('theTitle')
+
+
+  newTitle.once('value').then(function(snapshot){
+    var temp = snapshot.val();
+    title.innerHTML = temp
+  })
+
+}
+// newTitle.on('value', function(snapshot){
+//   title.innerHTML = 'hello'
+// })
