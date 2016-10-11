665f9eeb641c1e7650761faf8722c221286c9794
Tim Pender <mrtimpender@gmail.com>
2016-10-11 02:08:42 -0600

    base show function working

--- public/js/db.js
+++ public/js/db.js
@@ -46,11 +46,49 @@ function saveRec(){
 
 function listRec() {
   firebaseDB.ref('users/'+firebase.auth().currentUser.uid + '/recObj').once('value', function(snapshot){
+    $('#recordingList').empty()
     snapshot.forEach(function(recObj){
       $('#recordingList').append(
         $('<li>').css('background-color', recObj.val().bgColor)
-        .append(recObj.val().name))
+        .attr('class', 'listedRec')
+        .attr('id', recObj.val().name)
+        .attr('onclick', 'showSavedRec(this);')
+        .append(recObj.val().name)
+        .append('</li>')
+      )
     })
   })
-  // $('#listRecordings').append(li)
+}
+
+function showSavedRec(el) {
+  var savedRecName = el.id
+  var recordingName = document.getElementById("recordingName")
+
+  $('#output').empty()
+  $('#recordingName').empty()
+
+  firebaseDB.ref('users/'+firebase.auth().currentUser.uid +'/recordings/'+ savedRecName).once('value', function(snapshot){
+    theRecording = snapshot.val().recordingArray;
+
+    recObj.buffer = theRecording
+    recObj.length = theRecording.length;
+    recObj.name = savedRecName
+    // TODO: Check on the bg color and ensure the object gets updated before rendering the image
+    $('#recordingName').text(savedRecName)
+    firebaseDB.ref('users/'+firebase.auth().currentUser.uid +'/recObj/'+savedRecName).once('value', function(snap){
+      recObj.bgColor = snap.val().bgColor
+    })
+
+    // Raw D3+SVG support.  Currently too browser intensive.
+    // TODO: Move calc to serverside.  Find way to parse width, match file, and re-display
+    if (theRecording.length <=150000) {
+      d3Buffer(theRecording);
+    } else {
+    //
+    // DRAWS THE CANVAS
+      d3CanvasBuff(theRecording);
+      recordingName.innerHTML = recObj.name
+    }
+
+  })
 }
